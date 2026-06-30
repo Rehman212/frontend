c5a032ce8659b18d61e192c466c9ccfc58c650d7
Rehman212 <Rehmanwebs@gmail.com>
2026-06-30 15:14:29 +0500

    New Changes1

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProdBuild = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  ...(isProdBuild ? { output: "export" as const } : {}),
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
